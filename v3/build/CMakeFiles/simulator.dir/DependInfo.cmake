deeb284dfb6a26e5f577822388b5a9cb8ceaf927
Emanuele Rucci <rucci.emanuele.work@gmail.com>
2025-08-20 16:11:46 +0200

    Docking end effector to prehensile point done

---
+++ v3/build/CMakeFiles/simulator.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/arm.cpp" "CMakeFiles/simulator.dir/src/arm.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/arm.cpp.o.d"
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/car.cpp" "CMakeFiles/simulator.dir/src/car.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/car.cpp.o.d"
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/freeflying.cpp" "CMakeFiles/simulator.dir/src/freeflying.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/freeflying.cpp.o.d"
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/lidar.cpp" "CMakeFiles/simulator.dir/src/lidar.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/lidar.cpp.o.d"
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/object.cpp" "CMakeFiles/simulator.dir/src/object.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/object.cpp.o.d"
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/simulator.cpp" "CMakeFiles/simulator.dir/src/simulator.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/simulator.cpp.o.d"
+  "/Users/emanuelerucci/Desktop/Sapienza/Materie/RobProg2025/simulator/v3/src/world.cpp" "CMakeFiles/simulator.dir/src/world.cpp.o" "gcc" "CMakeFiles/simulator.dir/src/world.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
