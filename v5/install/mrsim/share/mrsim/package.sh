d2de69e3301383379514f74669db57f1074132e7
Emanuele Rucci <rucci.emanuele.work@gmail.com>
2025-08-30 10:51:43 +0000

    Add build type ament for ros 2 in the package for correct compiling

--- v5/install/mrsim/share/mrsim/package.sh
+++ v5/install/mrsim/share/mrsim/package.sh
@@ -79,9 +79,7 @@ _colcon_package_sh_source_script() {
 
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mrsim/hook/cmake_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mrsim/hook/ros_package_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mrsim/hook/pkg_config_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mrsim/hook/pkg_config_path_multiarch.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/mrsim/local_setup.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
